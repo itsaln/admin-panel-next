05f0ba4879f15b06fd050e5d73308a1c1831959b
itsaln <itsaln23@gmail.com>
2022-10-03 18:00:39 +0500

    02

--- app/shared/interfaces/movie.interfaces.ts
+++ app/shared/interfaces/movie.interfaces.ts
@@ -2,7 +2,7 @@ import { IReview } from '@/shared/interfaces/review.interface'
 
 export interface IMovie {
 	name: string
-	rating: number
+	rating: number | null
 	poster: string
 	views: number
 	reviews?: IReview[]
